1736c7bf9411b702a793da2e65481f68322601cf
Alex <dizzersee@gmail.com>
2024-03-21 13:09:33 +0100

    logout

--- routes/api.php
+++ routes/api.php
@@ -5,3 +5,13 @@ use App\Http\Controllers\AuthController;
 
 Route::post('login', [AuthController::class, 'login']);
 Route::post('register', [AuthController::class, 'register']);
+
+Route::middleware(['auth:api'])->group(function() {
+
+    Route::get('logout', [AuthController::class, 'logout']);
+
+    Route::get('testroute', function() {
+        return "Diese Route ist nur für authentifizierte Benutzer zugänglich!";
+    });
+
+});
